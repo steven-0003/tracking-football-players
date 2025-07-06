af2ee22e2aae67b77a32582d5e34677e769a2b46
Steven <stevenmoussa03@gmail.com>
2025-07-06 17:03:48 +0100

    Refactor code for loading videos
    Move code for loading videos for hiera and slowfast models to video utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-from .video_utils import read_video, save_video
+from .video_utils import read_video, save_video, format_frames, frames_from_video_file
 from .bbox_utils import get_bbox_center, get_bbox_width, measure_distance, get_foot_position
 from .track_utils import player_tracks_by_frames, player_tracks_by_ids, remove_short_tracks, get_transformed
--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,5 +1,7 @@
 import cv2
 from tqdm import tqdm
+import random
+import numpy as np
 
 def read_video(path: str) -> list[cv2.Mat]:
     """Reads a video file and returns its frames as a list of numpy arrays."""
@@ -21,3 +23,42 @@ def save_video(path: str, frames, w, h, num_frames) -> None:
     for frame in tqdm(frames,desc=f"Saving video in {path}", total=num_frames):
         out.write(frame)
     out.release()
+
+def format_frames(frame, output_size):
+    """Format frames to tensor with specified size"""
+    frame = cv2.resize(frame, output_size)
+    frame = frame / 255.0
+    return frame
+
+def frames_from_video_file(video_path, n_frames, output_size=(224, 224), frame_step=4):
+    """Extract frames from video file"""
+    result = []
+    src = cv2.VideoCapture(str(video_path))
+
+    video_length = src.get(cv2.CAP_PROP_FRAME_COUNT)
+    need_length = 1 + (n_frames - 1) * frame_step
+
+    if need_length > video_length:
+        start = 0
+    else:
+        max_start = video_length - need_length
+        start = random.randint(0, max_start + 1)
+
+    src.set(cv2.CAP_PROP_POS_FRAMES, start)
+    ret, frame = src.read()
+    result.append(format_frames(frame, output_size))
+
+    for _ in range(n_frames - 1):
+        for _ in range(frame_step):
+            ret, frame = src.read()
+        if ret:
+            frame = format_frames(frame, output_size)
+            result.append(frame)
+        else:
+            result.append(np.zeros_like(result[0]))
+
+    src.release()
+
+    result = np.array(result)
+    result = np.transpose(result, (3, 0, 1, 2))  # (T, C, H, W) format
+    return result
